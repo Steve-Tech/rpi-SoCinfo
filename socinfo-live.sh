1cba81e87b0f7cd3348268cf7b240eb08f0125bc
Steve Tech <supercomputer008@gmail.com>
2017-09-22 14:40:39 +1000

    Add files via upload

--- socinfo-live.sh
+++ socinfo-live.sh
@@ -5,7 +5,7 @@ clear
 while true
 do
 echo
-echo "Temp=\t$(cat /sys/class/thermal/thermal_zone0/temp)\t$(vcgencmd measure_temp)"
+echo "CPU Temp=$(cat /sys/class/thermal/thermal_zone0/temp)\t$(vcgencmd measure_temp)"
 echo "Freq=\t$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)\t$(vcgencmd measure_volts)"
 echo
 date
--- socinfo.sh
+++ socinfo.sh
@@ -6,7 +6,7 @@ do
 date
 echo "Freq:"
 cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq
-echo "Temp:"
+echo "CPU Temp:"
 cat /sys/class/thermal/thermal_zone0/temp
 vcgencmd measure_temp 
 vcgencmd measure_volts
